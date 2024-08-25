4f4b07bbba0b47b74838b4ac55ece0559c4c0ef4
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-08-25 20:06:23 +0300

    Updating TcpClient method sendAndReceive for proper working with TCP

--- src/main/java/telran/net/TcpClient.java
+++ src/main/java/telran/net/TcpClient.java
@@ -3,79 +3,96 @@ import java.io.*;
 import java.net.*;
 import java.time.Instant;
 import static telran.net.TcpConfigurationProperties.*;
-
 import org.json.JSONObject;
 public class TcpClient implements Closeable{
- private static final long DEFAULT_INTERVAL = 3000;
-private static final int DEFAULT_NUMBER_ATTEMPTS = 10;
-String hostName ;
- int port;
- Socket socket;
- BufferedReader receiver;
- PrintStream sender;
- long interval;
- int nAttempts;
-public TcpClient(String hostName, int port, long interval, int nAttempts) {
-	this.hostName = hostName;
-	this.port = port;
-	this.interval = interval;
-	this.nAttempts = nAttempts;
-	if(this.nAttempts < 0) {
-		this.nAttempts = 0;
-	}
-	connect();
-}
-public TcpClient(String hostName, int port) {
-	this(hostName,port,DEFAULT_INTERVAL,DEFAULT_NUMBER_ATTEMPTS);
-}
-private void connect() {
-	int counter = nAttempts;
-	do {
-		try {
-			socket = new Socket(hostName,port);
-			sender = new PrintStream(socket.getOutputStream());
-			receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			counter = 0;
-		} catch (IOException e) {
-			waitForInterval();
-			counter--;
+	private static final long DEFAULT_INTERVAL = 3000;
+	private static final int DEFAULT_NUMBER_ATTEMPTS = 10;
+	String hostName;
+	int port;
+	Socket socket;
+	BufferedReader receiver;
+	PrintStream sender;
+	long interval;
+	int nAttempts;
+	public TcpClient(String hostName, int port, long interval, int nAttempts) {
+		this.hostName = hostName;
+		this.port = port;
+		this.interval = interval;
+		this.nAttempts = nAttempts;
+		if(this.interval < 0) {
+			this.interval = 0;
 		}
-	} while (counter!=0);
-	
-}
-private void waitForInterval() {
-	Instant finished = Instant.now().plusMillis(interval);
-	while(Instant.now().isBefore(finished)) {
+		connect();
+	}
+	private void connect() {
+		int counter = nAttempts;
+		do {
+			try {
+				socket = new Socket(hostName, port);
+				sender = new PrintStream(socket.getOutputStream());
+				receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				counter = 0;
+				
+			}catch(IOException e) {
+				waitForInterval();
+				counter--;
+			}
+			
+		}while(counter != 0);
 		
 	}
-}
-@Override
-public void close() throws IOException {
-	try {
-		socket.close();
-	} catch (IOException e) {
-		throw new RuntimeException();
+	private void waitForInterval() {
+		Instant finished = Instant.now().plusMillis(interval);
+		while(Instant.now().isBefore(finished)) {
+			
+		}
+		
 	}
-	
-}
- public String sendAndReceive(Request request) {
-	 try {
-		 sender.println(request);
-		 String responseJSON = receiver.readLine();
-		 if(responseJSON == null) {
-			 throw new RuntimeException("Server closed connection");
-		 }
-		 JSONObject jsonObj = new JSONObject(responseJSON);
-		 ResponseCode responseCode =jsonObj.getEnum(ResponseCode.class, RESPONSE_CODE_FIELD);
-		 String responseData = jsonObj.getString(RESPONSE_DATA_FIELD);
-		 if(responseCode != ResponseCode.OK) {
-			 throw new RuntimeException(responseData);
-		 }
-		 return responseData;
-	} catch (IOException e) {
-		connect();
-		throw new RuntimeException("Server is unavailable,repeat later on");
+	public TcpClient(String hostName, int port) {
+		this(hostName, port, DEFAULT_INTERVAL, DEFAULT_NUMBER_ATTEMPTS);
 	}
-	 
- }
+	@Override
+	public void close()  {
+		try {
+			socket.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	public String sendAndReceive(Request request) {
+		try {
+			if(socket == null) {
+				connect();
+			}
+			sender.println(request);
+			String responseJSON = receiver.readLine();
+			if (responseJSON == null) {
+				terminateSocket();
+				throw new RuntimeException("Server closed connection");
+			}
+			JSONObject jsonObj = new JSONObject(responseJSON);
+			ResponseCode responseCode = jsonObj.getEnum(ResponseCode.class,
+					RESPONSE_CODE_FIELD);
+			String responseData = jsonObj.getString(RESPONSE_DATA_FIELD);
+			if(responseCode != ResponseCode.OK) {
+				throw new RuntimeException(responseData);
+			}
+			return responseData;
+
+		} catch (IOException e) {
+			terminateSocket();
+			throw new RuntimeException("Server is unavailable, repeat later on");
+		}
+	}
+	private void terminateSocket() {
+		try {
+			socket.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+		socket = null;
+	}
+
+
 }
